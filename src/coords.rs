4212d8c4e5c12dd30dce7e71b1d1879a1f96a2ec
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-03-24 12:05:04 +0000

    - Addition of Span coordinate type

--- src/coords.rs
+++ src/coords.rs
@@ -2,9 +2,9 @@
 
 use std::fmt::{Display, Formatter};
 
-///  The main coordinate struct
+/// A [Coord] represents a single location within the parser input
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct ParserCoords {
+pub struct Coords {
     /// The absolute character position
     pub absolute: usize,
     /// The row position
@@ -13,34 +13,34 @@ pub struct ParserCoords {
     pub column: usize,
 }
 
-/// Extract the line number from a [ParserCoords]
+/// Extract the line number from a [Coords]
 #[macro_export]
 macro_rules! line {
     ($coords : expr) => {
         $coords.line
-    }
+    };
 }
 
-/// Extract the column number from a [ParserCoords]
+/// Extract the column number from a [Coords]
 #[macro_export]
 macro_rules! column {
     ($coords : expr) => {
         $coords.column
-    }
+    };
 }
 
-/// Extract the absolute number from a [ParserCoords]
+/// Extract the absolute number from a [Coords]
 #[macro_export]
 macro_rules! absolute {
     ($coords : expr) => {
         $coords.absolute
-    }
+    };
 }
 
-impl Default for ParserCoords {
+impl Default for Coords {
     /// The default set of coordinates are positioned at the start of the first row
     fn default() -> Self {
-        ParserCoords {
+        Coords {
             absolute: 0,
             line: 1,
             column: 0,
@@ -48,16 +48,40 @@ impl Default for ParserCoords {
     }
 }
 
-impl Display for ParserCoords {
+impl Display for Coords {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{},{},({})]", self.line, self.column, self.absolute)
+        write!(
+            f,
+            "(line: {}, column: {}, absolute: {})",
+            self.line, self.column, self.absolute
+        )
     }
 }
 
-impl std::ops::Sub for ParserCoords {
+impl std::ops::Sub for Coords {
     type Output = usize;
     /// Subtraction is based on the absolute position, could be +/-ve
     fn sub(self, rhs: Self) -> Self::Output {
         self.absolute - rhs.absolute
+    }
+}
+
+/// A [Span] represents a linear interval within the parser input
+pub struct Span {
+    /// Start [Coords] for the span
+    start: Coords,
+    /// End [Coords] for the span
+    end: Coords,
+}
+
+impl Display for Span {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "start: {}, end: {}, length: {}",
+            self.start,
+            self.end,
+            self.end - self.start
+        )
     }
 }
