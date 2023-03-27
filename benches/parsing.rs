ea056451a47a0afa473c617896c6266e4c5106c2
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-03-27 18:40:57 +0100

    Wip benchmarks

---
+++ benches/parsing.rs
@@ -0,0 +1,39 @@
+use chisel_json::parser::Parser;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fs::File;
+use std::io::BufReader;
+
+macro_rules! build_parse_benchmark {
+    ($func : tt, $filename : expr) => {
+        fn $func() {
+            let f = File::open(format!("fixtures/json/{}.json", $filename)).unwrap();
+            let reader = BufReader::new(f);
+            let parser = Parser::default();
+            let _ = parser.parse(reader);
+        }
+    };
+}
+
+build_parse_benchmark!(blog_entries, "blog_entries");
+build_parse_benchmark!(simple_structure, "simple_structure");
+build_parse_benchmark!(bc_block, "bc_block");
+
+fn benchmark_blog_entries(c: &mut Criterion) {
+    c.bench_function("parse of blog_entries", |b| b.iter(blog_entries));
+}
+
+fn benchmark_simple_structure(c: &mut Criterion) {
+    c.bench_function("parse of simple_structure", |b| b.iter(simple_structure));
+}
+
+fn benchmark_bc_block(c: &mut Criterion) {
+    c.bench_function("parse of bc_block", |b| b.iter(bc_block));
+}
+
+criterion_group!(
+    benches,
+    benchmark_blog_entries,
+    benchmark_simple_structure,
+    benchmark_bc_block
+);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+#![feature(unicode_internals)]
 #![allow(unused_imports)]
 #![allow(dead_code)]
+
+extern crate core;
 
 pub mod coords;
 pub mod errors;
 mod lexer;
 pub mod parser;
-mod scanner;
+pub mod scanner;
 #[cfg(test)]
 mod test_macros;
