8b7464925402cc2228902f3cacb29c1cb5656695
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-13 14:58:16 +0100

    - Added standard JSON parsing benchmark sources - Updated benchmarking logic

--- benches/parsing.rs
+++ benches/parsing.rs
@@ -7,7 +7,7 @@ use std::io::BufReader;
 macro_rules! build_parse_benchmark {
     ($func : tt, $filename : expr) => {
         fn $func() {
-            let f = File::open(format!("fixtures/json/valid/{}.json", $filename)).unwrap();
+            let f = File::open(format!("fixtures/json/bench/{}.json", $filename)).unwrap();
             let reader = BufReader::new(f);
             let parser = Parser::default();
             let _ = parser.parse(reader);
@@ -15,50 +15,25 @@ macro_rules! build_parse_benchmark {
     };
 }
 
-build_parse_benchmark!(blog_entries, "blog_entries");
-build_parse_benchmark!(simple_structure, "simple_structure");
-build_parse_benchmark!(bc_block, "bc_block");
-build_parse_benchmark!(gh_emojis, "gh_emojis");
-build_parse_benchmark!(historical_events, "historical_events");
-build_parse_benchmark!(events, "events");
 build_parse_benchmark!(twitter, "twitter");
+build_parse_benchmark!(canada, "canada");
+build_parse_benchmark!(citm_catalog, "citm_catalog");
 
-fn benchmark_blog_entries(c: &mut Criterion) {
-    c.bench_function("parse of blog_entries", |b| b.iter(blog_entries));
-}
-
-fn benchmark_simple_structure(c: &mut Criterion) {
-    c.bench_function("parse of simple_structure", |b| b.iter(simple_structure));
-}
-
-fn benchmark_bc_block(c: &mut Criterion) {
-    c.bench_function("parse of bc_block", |b| b.iter(bc_block));
-}
-
-fn benchmark_gh_emojis(c: &mut Criterion) {
-    c.bench_function("parse of gh_emojis", |b| b.iter(gh_emojis));
-}
-
-fn benchmark_historical_events(c: &mut Criterion) {
-    c.bench_function("parse of historical events", |b| b.iter(historical_events));
-}
-
-fn benchmark_events(c: &mut Criterion) {
-    c.bench_function("parse of events", |b| b.iter(events));
+fn benchmark_citm_catalog(c: &mut Criterion) {
+    c.bench_function("parse of citm_catalog", |b| b.iter(citm_catalog));
 }
 
 fn benchmark_twitter(c: &mut Criterion) {
     c.bench_function("parse of twitter", |b| b.iter(twitter));
 }
+
+fn benchmark_canada(c: &mut Criterion) {
+    c.bench_function("parse of canada", |b| b.iter(canada));
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = benchmark_blog_entries,
-    benchmark_simple_structure,
-    benchmark_bc_block,
-    benchmark_gh_emojis,
-    benchmark_historical_events,
-    benchmark_events,
-    benchmark_twitter
+    targets = benchmark_citm_catalog, benchmark_twitter, benchmark_canada
 }
 criterion_main!(benches);
