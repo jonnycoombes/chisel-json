29e53c18c0df002d6b518c010b091db1e8a5171c
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-15 09:54:00 +0100

    Started scaffolding

--- src/events.rs
+++ src/events.rs
@@ -1,17 +1,38 @@
+use crate::coords::Span;
+use crate::errors::Error;
 use std::borrow::Cow;
 
-/// Enumeration of the various different event types emitted by the parser
-pub enum EventType {
+/// Enumeration of the various different matches that can be produced during a parse
+pub enum Match<'a> {
+    StartOfInput,
+    EndOfInput,
     StartObject,
+    ObjectKey(Cow<'a, str>),
     EndObject,
     StartArray,
+    ArrayString(usize, Cow<'a, str>),
+    ArrayNum(usize, f64),
+    ArrayBool(usize, bool),
+    ArrayNull(usize),
     EndArray,
+    String(Cow<'a, str>),
+    Num(f64),
+    Bool(bool),
+    Null,
 }
 
+/// A general event produced by the parser during a parse
 pub struct Event<'a> {
-    /// The type of the event
-    pub event_type: EventType,
+    /// The [Match] associated with the event
+    pub matched: Match<'a>,
 
-    /// An in-document path for relating to the event
-    pub path: Cow<'a, str>,
+    /// The [Span] associated with the [matched]
+    pub span: Span,
+}
+
+/// Trait that should be implemented by anything sinking events from the parser
+pub trait EventSink {
+    fn on_parse_event(event: &Event);
+
+    fn on_parse_error(error: &Error);
 }
