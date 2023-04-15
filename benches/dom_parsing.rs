d011bd6df3e97dc88bb2886f93502e176e22bd12
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-15 09:51:57 +0100

    Tidied up

--- benches/dom_parsing.rs
+++ benches/dom_parsing.rs
@@ -1,4 +1,4 @@
-use chisel_json::parser::Parser;
+use chisel_json::parser::DomParser;
 use criterion::{criterion_group, criterion_main, Criterion};
 use pprof::criterion::{Output, PProfProfiler};
 use std::fs::File;
@@ -9,8 +9,8 @@ macro_rules! build_parse_benchmark {
         fn $func() {
             let f = File::open(format!("fixtures/json/bench/{}.json", $filename)).unwrap();
             let reader = BufReader::new(f);
-            let parser = Parser::default();
-            let _ = parser.parse_dom(reader);
+            let parser = DomParser::default();
+            let _ = parser.parse(reader);
         }
     };
 }
@@ -20,7 +20,6 @@ build_parse_benchmark!(canada, "canada");
 build_parse_benchmark!(citm_catalog, "citm_catalog");
 build_parse_benchmark!(simple, "simple");
 build_parse_benchmark!(schema, "schema");
-build_parse_benchmark!(rota, "rota");
 
 fn benchmark_citm_catalog(c: &mut Criterion) {
     c.bench_function("DOM parse of citm_catalog", |b| b.iter(citm_catalog));
@@ -42,13 +41,9 @@ fn benchmark_schema(c: &mut Criterion) {
     c.bench_function("DOM parse of schema", |b| b.iter(schema));
 }
 
-fn benchmark_rota(c: &mut Criterion) {
-    c.bench_function("DOM parse of rota", |b| b.iter(rota));
-}
-
 criterion_group! {
     name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = benchmark_citm_catalog, benchmark_twitter, benchmark_canada, benchmark_simple, benchmark_schema, benchmark_rota
+    targets = benchmark_citm_catalog, benchmark_twitter, benchmark_canada, benchmark_simple, benchmark_schema
 }
 criterion_main!(benches);
