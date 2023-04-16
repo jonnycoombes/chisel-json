ccd82a8a07e6321f7db821e04963675c94346ed6
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-16 10:07:47 +0100

    Work in progress

--- src/events.rs
+++ src/events.rs
@@ -4,20 +4,29 @@ use std::borrow::Cow;
 
 /// Enumeration of the various different matches that can be produced during a parse
 pub enum Match<'a> {
+    /// Start of the input Emitted prior to anything else
     StartOfInput,
+    /// End of the input Emitted after everything else
     EndOfInput,
+    /// Emitted when the start of a new object is matched
     StartObject,
+    /// Emitted when a new key within an object is matched
     ObjectKey(Cow<'a, str>),
+    /// Emitted after an object has been fully parsed
     EndObject,
+    /// Emitted when the start of an array is matched
     StartArray,
-    ArrayString(usize, Cow<'a, str>),
-    ArrayNum(usize, f64),
-    ArrayBool(usize, bool),
-    ArrayNull(usize),
+    /// Emitted when the end of an array is matched
     EndArray,
+    /// Emitted when a string is matched
     String(Cow<'a, str>),
-    Num(f64),
+    /// Emitted when an integer is matched
+    Integer(i64),
+    /// Emitted when a float is matched
+    Float(f64),
+    /// Emitted when a boolean is matched
     Bool(bool),
+    /// Emitted when a null is matched
     Null,
 }
 
@@ -32,7 +41,9 @@ pub struct Event<'a> {
 
 /// Trait that should be implemented by anything sinking events from the parser
 pub trait EventSink {
+    /// Called when the parser emits a new event
     fn on_parse_event(event: &Event);
 
+    /// Called when the parser encounters an error
     fn on_parse_error(error: &Error);
 }
