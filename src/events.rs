38bfab6f872adbfdeb43c57cff4898521643f834
JC <jcoombes@jcs-software.co.uk>
2023-04-21 19:05:42 +0100

    32 internalise path information to event struct (#33)
    * Simplified signature for event closure
    * Updated macro defs to take into account optional path component
    * Modified event generation at start of stream to emit no path information
    * Modified benchmark to take into account simplified event closure
    signature
    * Fixed error in rustdoc
    * Added noddy implementations of `Display`
    * Created a helper method to check whether a path points to an array
    * Minor changes?
    * Added test for array path checking
    * Added helper method for detecting `Event` instances that contain a path
    * Tags updated
    * Bumped version number
    * Removed benchmarks from standard CI build

--- src/events.rs
+++ src/events.rs
@@ -1,9 +1,10 @@
 use crate::coords::Span;
 use crate::errors::Error;
+use crate::paths::JsonPath;
 use std::borrow::Cow;
+use std::fmt::Display;
 
 /// Enumeration of the various different matches that can be produced during a parse
-#[derive(Debug)]
 pub enum Match<'a> {
     /// Start of the input Emitted prior to anything else
     StartOfInput,
@@ -31,12 +32,56 @@ pub enum Match<'a> {
     Null,
 }
 
+impl<'a> Display for Match<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Match::StartOfInput => write!(f, "StartOfInput"),
+            Match::EndOfInput => write!(f, "EndOfInput"),
+            Match::StartObject => write!(f, "StartObject"),
+            Match::ObjectKey(_) => write!(f, "ObjectKey"),
+            Match::EndObject => write!(f, "EndObject"),
+            Match::StartArray => write!(f, "StartArray"),
+            Match::EndArray => write!(f, "EndArray"),
+            Match::String(value) => write!(f, "String({})", value),
+            Match::Integer(value) => write!(f, "Integer({})", value),
+            Match::Float(value) => write!(f, "Float({})", value),
+            Match::Boolean(b) => write!(f, "Boolean({})", b),
+            Match::Null => write!(f, "Null"),
+        }
+    }
+}
+
 /// A general event produced by the parser during a parse
-#[derive(Debug)]
 pub struct Event<'a> {
     /// The [Match] associated with the event
     pub matched: Match<'a>,
 
     /// The [Span] associated with the [matched]
     pub span: Span,
+
+    /// Optional [JsonPath] information relating to the event
+    pub path: Option<&'a JsonPath<'a>>,
+}
+
+impl<'a> Event<'a> {
+    /// Checks whether an event has a path or not
+    fn has_path(&self) -> bool {
+        self.path.is_some()
+    }
+}
+
+impl<'a> Display for Event<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.path.is_some() {
+            write!(
+                f,
+                "Event[{}, {}, {}]",
+                self.matched,
+                self.span,
+                self.path.unwrap()
+            )
+        } else {
+            write!(f, "Event[{}, {}]", self.matched, self.span)
+        }
+    }
 }
