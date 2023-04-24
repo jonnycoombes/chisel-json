8ebf5db68ef4305844920a058f99287d6dc59977
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-24 15:20:58 +0100

    Minor formatting changes

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,10 @@
 //!
 //! It's a bare-bones parser that is intended to allow you to choose how you want to parse a lump of *cursed* JSON,
 //! and then either build/transform a DOM into a richer AST structure, or alternatively just cherry-pick the useful
-//! bits of the payload via closures which are called in response to SAX parsing events. (Because *let's face it*, JSON
-//! payloads usually come burdened with a whole load of unnecessary crap that you'll never use).
+//! bits of the payload via closures which are called in response to SAX parsing events.
+//!
+//! (Because *let's face it*, JSON payloads usually come burdened with a whole load of unnecessary crap that
+//! you'll never use).
 //!
 #![allow(unused_imports)]
 #![allow(dead_code)]
