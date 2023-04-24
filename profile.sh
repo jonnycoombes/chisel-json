016b5415e2071f40b84ab4d38930fd4f47299642
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-24 14:25:03 +0100

    Added DOM and SAX parsing profiling jobs as separate things. Added a warning to the script because a complete profile will take a very long time

--- profile.sh
+++ profile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+ansiRed='\033[0;31m'
 ansiGreen='\033[0;32m'
 ansiLightGrey='\033[1;37m'
 ansiNoColour='\033[0m'
@@ -24,15 +25,27 @@ fi
 cd "$(dirname "$0")"
 
 main() {
-	echo -e "$ansiGreen"
-	echo 'Running lexer profiling for 60 seconds per benchmark'
-	echo -e "$ansiNoColour"
-	cargo bench --bench lexing -- --profile-time 60
+
+  echo -e "$ansiRed"
+  echo '*** WARNING *** Profiling everything will take quite some time'
+  echo -e "$ansiNoColour"
+
+  benchmarkTime=30
 
 	echo -e "$ansiGreen"
-	echo 'Running parser profiling for 60 seconds per benchmark'
+	echo 'Running lexer profiling for ' $benchmarkTime ' seconds per benchmark'
 	echo -e "$ansiNoColour"
-	cargo bench --bench parsing -- --profile-time 60
+	cargo bench --bench lexing -- --profile-time $benchmarkTime
+
+	echo -e "$ansiGreen"
+	echo 'Running DOM parser profiling for ' $benchmarkTime ' seconds per benchmark'
+	echo -e "$ansiNoColour"
+	cargo bench --bench dom_parsing -- --profile-time $benchmarkTime
+
+	echo -e "$ansiGreen"
+	echo 'Running SAX parser profiling for ' $benchmarkTime ' seconds per benchmark'
+	echo -e "$ansiNoColour"
+	cargo bench --bench sax_parsing -- --profile-time 60
 }
 
 main "$@"
