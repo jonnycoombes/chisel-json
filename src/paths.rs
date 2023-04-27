79e5267d9a230b8741eef4e59522bf9cb0de7a55
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-27 16:09:47 +0100

    Changed default serialisation for paths to be using quoted name identifiers

--- src/paths.rs
+++ src/paths.rs
@@ -29,7 +29,7 @@ impl<'a> Display for JsonPathComponent<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Root => write!(f, "$"),
-            Self::NameSelector(s) => write!(f, "{}", s),
+            Self::NameSelector(s) => write!(f, "['{}']", s),
             Self::WildcardSelector => write!(f, "[*]"),
             Self::IndexSelector(i) => write!(f, "[{}]", i),
             Self::RangeSelector(i, j) => write!(f, "[{}..{}]", i, j),
