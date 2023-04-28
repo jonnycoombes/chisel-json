7ccb824cd88ac3797367587c6e9b770b87b74b28
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-04-28 08:09:10 +0100

    Simple path extraction example

--- examples/distinct_paths.rs
+++ examples/distinct_paths.rs
@@ -1,31 +1,27 @@
-use std::{collections::BTreeSet, ops::Deref, time::Instant};
+use chisel_json::{decoders::Encoding, events::Match, sax::Parser};
 
-use chisel_json::{decoders::Encoding, sax::Parser};
+/// Macro to tidy up the match arm
+macro_rules! selected_event {
+    () => {
+        Match::StartObject
+            | Match::StartArray
+            | Match::String(_)
+            | Match::Integer(_)
+            | Match::Float(_)
+            | Match::Boolean(_)
+            | Match::Null
+    };
+}
 
-/// Very simple example of using the SAX based parser to extract all the distinct object keys
-/// within a JSON document and stash them away into a BTree
+/// Very simple example of using the SAX based parser to extract all the distinct keys in a JSON
+/// document
 fn main() {
-    let start = Instant::now();
     let parser = Parser::with_encoding(Encoding::Utf8);
-    let mut distinct_paths: BTreeSet<String> = BTreeSet::new();
     let _result = parser.parse_file("fixtures/json/bench/citm_catalog.json", &mut |evt| {
-        match evt.path {
-            Some(p) => {
-                if !distinct_paths.contains(p.as_string().deref()) {
-                    match evt.matched {
-                        chisel_json::events::Match::ObjectKey(_) => {
-                            distinct_paths.insert(p.as_string().to_string());
-                        }
-                        _ => (),
-                    };
-                }
-            }
+        match evt.matched {
+            selected_event!() => println!("{}", evt.path.unwrap()),
             _ => (),
         }
         Ok(())
     });
-    distinct_paths
-        .iter()
-        .for_each(|path| println!("Found distinct path: {}", path));
-    println!("Extract all unique paths in: {:?}", start.elapsed());
 }
